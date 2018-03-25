570b0138aee8defa29cc24b1438aed8912f561c3
Michal Takac <takacmichal92@gmail.com>
2018-03-25 18:51:37 +0200

    move html files under `/web` folder

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -25,9 +25,9 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "web/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "web/404.html")
     };
 
     let mut file = File::open(filename).unwrap();
